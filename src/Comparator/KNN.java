677e046a5352ebc7103174f7d3fa2167826a3ec4
arunvyasnarayanan <aruvyas@apple.com>
2024-03-18 21:02:17 -0700

    k closet Number to given number

---
+++ src/Comparator/KNN.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.PriorityQueue;
+
+public class KNN {
+
+    public int[][]KClosest(int[][] points, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new CustomComparator(k));
+        for(int num : points){
+
+        }
+    }
+
+    public static void main (String[] args){
+
+    }
+}
