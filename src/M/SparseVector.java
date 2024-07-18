7fb71293e07c80a2824d29bc0aaf6b62c19c192c
arunvyasnarayanan <aruvyas@apple.com>
2024-07-17 17:10:23 -0700

    Used HashMap to solve this

--- src/M/SparseVector.java
+++ src/M/SparseVector.java
@@ -24,42 +24,31 @@ v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
 */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SparseVector {
-    ArrayList<int[]> arrayList = new ArrayList<>();
+
+    HashMap<Integer, Integer> map;
     SparseVector(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-                this.arrayList.add(new int[]{i,nums[i]});
+        map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                map.put(i, nums[i]);
             }
         }
     }
 
     // Return the dotProduct of two sparse vectors
-    public int dotProduct(SparseVector vector2) {
-        int i = 0;
-        int j = 0;
-        ArrayList<Integer> productList = new ArrayList<>();
-        while ((i<this.arrayList.size())&&(j<vector2.arrayList.size())){
-            if(this.arrayList.get(i)[0] == vector2.arrayList.get(j)[0]){
-                int sum  = this.arrayList.get(i)[1] * vector2.arrayList.get(j)[1];
-                productList.add(sum);
-                i++;
-                j++;
-            }else if(this.arrayList.get(i)[0] < vector2.arrayList.get(j)[0]){
-                i++;
-            }else{
-                j++;
+    public int dotProduct(SparseVector vec) {
+        int sum = 0;
+        for(HashMap.Entry<Integer, Integer> entry: map.entrySet()){
+            int index = entry.getKey();
+            if(vec.map.containsKey(index)){
+                sum = sum + (vec.map.get(index)*entry.getValue());
             }
         }
-        int result = 0;
-        while(productList.size()>0){
-            result += productList.get(0);
-            productList.remove(0);
-        }
-        return result;
+        return sum;
     }
-
     public static void main(String[] args){
         int[] nums1 = new int[] {1,0,0,2,3};
         int[] nums2 = new int[] {0,3,0,4,0};
@@ -69,4 +58,37 @@ public class SparseVector {
         System.out.println(ans);
     }
 
+//    ArrayList<int[]> arrayList = new ArrayList<>();
+//    SparseVector(int[] nums) {
+//        for(int i=0;i<nums.length;i++){
+//            if(nums[i] != 0){
+//                this.arrayList.add(new int[]{i,nums[i]});
+//            }
+//        }
+//    }
+//
+//    // Return the dotProduct of two sparse vectors
+//    public int dotProduct(SparseVector vector2) {
+//        int i = 0;
+//        int j = 0;
+//        ArrayList<Integer> productList = new ArrayList<>();
+//        while ((i<this.arrayList.size())&&(j<vector2.arrayList.size())){
+//            if(this.arrayList.get(i)[0] == vector2.arrayList.get(j)[0]){
+//                int sum  = this.arrayList.get(i)[1] * vector2.arrayList.get(j)[1];
+//                productList.add(sum);
+//                i++;
+//                j++;
+//            }else if(this.arrayList.get(i)[0] < vector2.arrayList.get(j)[0]){
+//                i++;
+//            }else{
+//                j++;
+//            }
+//        }
+//        int result = 0;
+//        while(productList.size()>0){
+//            result += productList.get(0);
+//            productList.remove(0);
+//        }
+//        return result;
+//    }
 }
