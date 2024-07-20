830d38599054c7f321a9487fb5b20df6419ffbad
arunvyasnarayanan <aruvyas@apple.com>
2024-07-20 14:49:15 -0700

    revision done

--- src/Tree/TreeToGraphDistance.java
+++ src/Tree/TreeToGraphDistance.java
@@ -18,17 +18,17 @@ import java.util.List;
         DFS with visited set and depth
 */
 
-
-
-
-
 public class TreeToGraphDistance {
+    // Ask if values are unique
     HashMap<Integer, ArrayList<Integer>> graph;
     HashSet<Integer> visited;
     List<Integer> result;
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        // Build graph Adjacency matrix
         graph = new HashMap<>();
         buildGraph(root, null);
+
+        // DFS starts
         visited = new HashSet<>();
         result = new ArrayList<>();
         visited.add(target.value);
@@ -53,6 +53,7 @@ public class TreeToGraphDistance {
     public void dfs(int curr, int depth, int k){
         if(depth==k){
             result.add(curr);
+            return;
         }
         for(int neighbour: graph.getOrDefault(curr, new ArrayList<>())){
             if(!visited.contains(neighbour)){
@@ -61,4 +62,51 @@ public class TreeToGraphDistance {
             }
         }
     }
+
+
+
+
+
+    /*
+        Assuming we can add parent pointer
+    */
+
+    /*
+    List<Integer> result = new ArrayList<>();
+    HashSet<TreeNode> visited = new HashSet<>();
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        //Optional step only if we have to reconstruct parent pointers
+
+        addParent(root, null);  // step-1
+        //------------------
+
+
+        dfs(target, k);         // step-2
+        return result;          // step-3
+    }
+
+    public void addParent(TreeNode current, TreeNode parent){
+        if(current!=null){
+            current.parent = parent;
+            addParent(current.left, current);
+            addParent(current.right, current);
+        }
+    }
+
+    public void dfs(TreeNode current, int distance){
+        if((current==null)||(visited.contains(current))){
+            return;
+        }
+        visited.add(current);
+        if(distance==0){
+            result.add(current.val);
+            return;
+        }
+        dfs(current.parent, distance-1);
+        dfs(current.left, distance-1);
+        dfs(current.right, distance-1);
+    }
+    */
+
 }
