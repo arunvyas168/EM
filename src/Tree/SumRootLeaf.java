db1fa034950d13a22c6364494c33caf517416547
arunvyasnarayanan <aruvyas@apple.com>
2024-07-20 15:12:47 -0700

    revision done

--- src/Tree/SumRootLeaf.java
+++ src/Tree/SumRootLeaf.java
@@ -2,13 +2,16 @@ package Tree;
 
 
 /*
-    TLDR: We Just Traverse Tree and form numbers and add them
+    TLDR:
 
-    1. Use an util where number is passed as 0
-    2. It will check current nodes value and then append to number
-    3. If we reach the end leaf --> (right and left null) --> we send final number back
-    4. We have to add it recursively on left and right subtree
+        1. We need to pass number computed at each level to leaf
+        2. Once leaf is reached return the number
+        3. At top level we add
 
+    1. Use util and pass number as 0
+    2. compute number = (number*10)+value
+    3. if leaf noad return number;
+    4. Else call dfs and add the return value
 */
 
 
@@ -18,14 +21,21 @@ public class SumRootLeaf {
         if(root==null){
             return 0;
         }
+
+        // compute value
         num = num*10+root.value;
+
+        // return if leaf node
         if((root.left==null)&&(root.right==null)){
             return num;
         }
+
+        // Add left and right subtree to get sum
         return dfs(root.left, num) + dfs(root.right, num);
     }
 
     public int sumNumbers(TreeNode root) {
+        // most imp to understand to send 0 (10*0 = 0)
         return dfs(root, 0);
     }
 }
