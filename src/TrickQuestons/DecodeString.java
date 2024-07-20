e2303a3b80f902e752064be29178d52c076b7aeb
arunvyasnarayanan <aruvyas@apple.com>
2024-07-20 12:27:31 -0700

    Prefix sum. target <= prefixSum

--- src/TrickQuestons/DecodeString.java
+++ src/TrickQuestons/DecodeString.java
@@ -26,12 +26,12 @@ Output: "abcabccdcdcdef"
 /*
     Solution:
 
-    TLDR: use stack and then once you expand braces push it back to stack in order to expand later
-
+    TLDR:
         1. Use a stack
-        2. use for loop to push char to stack
+        2. We need to compute per bracket close and push it back to stack
+        3. use for loop to push char to stack
         3. Push and push til you see ']'
-        4. If you see a ']' call buildProductStack function
+        4. If you see a ']' compute -------> call seperate function function
                 buildProductStack:
                     1. create new sb
                     2. while stack not empty
@@ -42,7 +42,7 @@ Output: "abcabccdcdcdef"
                     7. keep appneding sb to new SB
                     8. push character to stack ----->> very imp
 
-        5. Pop the stack and insert in 0th place to REVERSE and return string
+        Note: Pop the stack and insert in 0th place to REVERSE and return string
 
 */
 
@@ -50,47 +50,68 @@ import java.util.Stack;
 
 public class DecodeString {
 
+    public static String decodeString(String encoded){
+        // use stack
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+
+        for(int i=0;i<encoded.length();i++){
+            // keep pushing till you see ']'
+            if(encoded.charAt(i)!=']'){
+                stack.push(encoded.charAt(i));
+            }else {
+                // when you see ']'
+                buildProductStack(stack);
+            }
+        }
+
+        // now stack contains the string---> since we are poping we need to do insert at 0
+        while (!stack.isEmpty()){
+            result.insert(0,stack.pop());
+        }
+
+        // return
+        return result.toString();
+    }
+
+
     public static void buildProductStack(Stack<Character> stack){
         StringBuilder sb = new StringBuilder();
+        // this is to find the number
         int number = 0;
+
+        // we pop till we find '[' to know what's the string
         while(!stack.isEmpty()){
             Character top = stack.pop();
             if(top!='['){
+                // since pop use insert 0
                 sb.insert(0, top);
             }else {
                 break;
             }
         }
+        // need place value if we are doing in reverse
         int placeValue = 1;
         while ((!stack.isEmpty())&&(Character.isDigit(stack.peek()))){
+            // place value will decide if its units, tens, hundreds
             number += Character.getNumericValue(stack.pop()) * placeValue;
             placeValue = placeValue*10;
         }
+
+        // post we have string and number, multiple string with number and push back in stack
         StringBuilder newSB = new StringBuilder();
         while(number>0){
             newSB.append(sb);
             number--;
         }
+
+        // need for loop to push one by one
         for(int i= 0; i<newSB.length(); i++){
             stack.push(newSB.charAt(i));
         }
     }
 
-    public static String decodeString(String encoded){
-        Stack<Character> stack = new Stack<>();
-        StringBuilder result = new StringBuilder();
-        for(int i=0;i<encoded.length();i++){
-            if(encoded.charAt(i)!=']'){
-                stack.push(encoded.charAt(i));
-            }else {
-                buildProductStack(stack);
-            }
-        }
-        while (!stack.isEmpty()){
-            result.insert(0,stack.pop());
-        }
-        return result.toString();
-    }
 
     public static void main(String[] args){
         String input = "3[a]2[bc]";
