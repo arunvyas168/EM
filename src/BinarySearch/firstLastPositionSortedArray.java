4ebe0425c12d975b55c7ca19e7b620f3af7fbb59
arunvyasnarayanan <aruvyas@apple.com>
2024-07-24 16:36:30 -0700

    clone graph

--- src/BinarySearch/firstLastPositionSortedArray.java
+++ src/BinarySearch/firstLastPositionSortedArray.java
@@ -5,6 +5,8 @@ If target is not found in the array, return [-1, -1].
 You must write an algorithm with O(log n) runtime complexity.
 */
 
+import java.util.Arrays;
+
 /*
         TLDR:
             1. Get pivot via binary search
@@ -13,7 +15,7 @@ You must write an algorithm with O(log n) runtime complexity.
         Note: its left+1 and right-1;
 */
 public class firstLastPositionSortedArray {
-    public int[] searchRange(int[] nums, int target) {
+    public static int[] searchRange(int[] nums, int target) {
         int pivot = binarySearch(nums, 0, nums.length-1, target);
         if (pivot==-1){
             return new int[] {-1, -1};
@@ -26,10 +28,11 @@ public class firstLastPositionSortedArray {
         while((right<nums.length)&&(nums[right]==target)){
             right++;
         }
+        //Remember that we get out of loop only after left is decremented and right is incremented.
         return new int[] {left+1, right-1};
     }
 
-    public int binarySearch(int[] nums, int start, int end, int target){
+    public static int binarySearch(int[] nums, int start, int end, int target){
         if(start>end){
             return -1;
         }
@@ -41,5 +44,10 @@ public class firstLastPositionSortedArray {
         }else{
             return binarySearch(nums, mid+1, end, target);
         }
+    }
+
+    public static void main(String[] args){
+        int[] input  = new int[] {5,7,7,8,8,10};
+        System.out.println(Arrays.toString(searchRange(input, 8)));
     }
 }
