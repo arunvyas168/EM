1a2e946bc4d543cecbf0adb5d7aeb54815728c4a
arunvyasnarayanan <aruvyas@apple.com>
2024-07-25 10:42:11 -0700

    ArrayList of Neighbours

--- src/Graph/Node.java
+++ src/Graph/Node.java
@@ -6,16 +6,19 @@ import java.util.List;
 public class Node {
     public int val;
     public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
+
+    public Node(int val, ArrayList<Node> neighbors) {
+        this.val = val;
+        this.neighbors = neighbors;
     }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
+
+//    public Node() {
+//        this.val = 0;
+//        this.neighbors = new ArrayList<>();
+//    }
+//    public Node(int val) {
+//        this.val = val;
+//        neighbors = new ArrayList<>();
+//    }
 }
+
