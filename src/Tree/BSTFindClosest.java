47b7524aac63369967e49f2a2df1420eb9e073ff
arunvyasnarayanan <aruvyas@apple.com>
2024-07-25 17:07:54 -0700

    update previous element

--- src/Tree/BSTFindClosest.java
+++ src/Tree/BSTFindClosest.java
@@ -3,9 +3,23 @@ package Tree;
 import java.util.Stack;
 
 /*
-    We will do Stack -- in order -- iterative
-    We will stop if we find the element itself
-    Otherwise we need one small and one bigger and compare
+    Given the root of a binary search tree and a target value, return the value in the BST that is closest to the target. If there are multiple answers, print the smallest.
+Example 1:
+Input: root = [4,2,5,1,3], target = 3.714286
+Output: 4
+
+Example 2:
+Input: root = [1], target = 4.428571
+Output: 1
+*/
+
+
+
+/*
+        TLDR:
+            InOrder BST --> Sorted Array
+            Stop traversal when you find lastMin <= target < current value
+            Use stack and do in order
 */
 
 public class BSTFindClosest {
