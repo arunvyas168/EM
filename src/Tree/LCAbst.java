b1b6f5353a3b9f75d74c722a682f3f133a88ac33
arunvyasnarayanan <aruvyas@apple.com>
2024-07-25 17:15:08 -0700

    update previous element

--- src/Tree/LCAbst.java
+++ src/Tree/LCAbst.java
@@ -9,8 +9,8 @@ package Tree;
 
          1. Take root node as current
          2. Traverse till its not null while(current!=null)
-         3. If value > both left and right --> current =  current.left;
-         4. If value < both left and right --> current =  current.right;
+         3. If value < both left and right --> current =  current.left;
+         4. If value > both left and right --> current =  current.right;
          5. else return current
          if not found return null
 */
